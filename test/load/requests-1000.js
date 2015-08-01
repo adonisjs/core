2f08745fe9d54b809392d6014027c9d06158b041
Harminder Virk <virk.officials@gmail.com>
2015-08-01 09:13:55 +0530

    Added load tests

---
+++ test/load/requests-1000.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/**
+ * @author      - Harminder Virk
+ * @package     - adonis-http-dispatcher
+ * @description - Loading testing the server
+ */
+
+const loadtest = require('loadtest');
+const options = {
+  url: 'http://localhost:3000',
+  maxRequests: 1000,
+  statusCallback: statusCallback
+};
+
+function statusCallback(result) {
+  // console.log('Current Status: ' + require('util').inspect(result));
+}
+
+const Dispatcher = require("../../index"),
+  Server = Dispatcher.Server,
+  Router = Dispatcher.Router
+
+
+Router.get("/", function*(request, response) {
+  response.send("passing>>>>");
+});
+
+Server.start(3000);
+
+console.log("Load testing with 1,000 requests .....")
+loadtest.loadTest(options, function(error, result) {
+  if (error) {
+    return console.error('Got an error: %s', error);
+  }
+  console.log(result);
+  console.log('Tests ran successfully');
+  Server.stop();
+});
