04c87731cc4f500d478a3ff57750d8853224a80d
Harminder Virk <virk.officials@gmail.com>
2015-08-17 13:32:03 +0530

    Added file class to manage file uploads

--- src/File/index.js
+++ src/File/index.js
@@ -6,8 +6,12 @@
  * @description - File class to add helpers methods on top of uploaded file
  */
 
-function File (files) {
-  this.files = files
+const path = require('path')
+const fsExtra = require('co-fs-extra')
+
+
+function File (file) {
+  this.file = file
 }
 
 /**
@@ -15,42 +19,75 @@ function File (files) {
  * registered public directory
  * @param {String} toPath
  */
-File.prototype.move = function (toPath) {}
+File.prototype.move = function * (toPath,name) {
+  let self = this
+  name = name || this.clientName()
+  const uploadingFileName = `${toPath}/${name}`
+  try{
+    yield fsExtra.copy(self.file.path,uploadingFileName)
+  }catch(e){
+    self.file.error = e
+  }
+}
 
 /**
  * returns mime type for uploaded file
  * @return {String}
  */
-File.prototype.mimeType = function () {}
+File.prototype.mimeType = function () {
+  return this.file['type']
+}
 
 /**
  * returns file extension
  * @return {String}
  */
-File.prototype.extension = function () {}
+File.prototype.extension = function () {
+  return path.extname(this.clientName()).replace('.','')
+}
 
 /**
  * returns client name for uploaded file
  * @return {String}
  */
-File.prototype.clientName = function () {}
+File.prototype.clientName = function () {
+  return this.file['name'];
+}
 
-/**
- * returns client extension for uploaded file
- * @return {String}
- */
-File.prototype.clientExtension = function () {}
 
 /**
  * returns client size for uploaded file
  * @return {String}
  */
-File.prototype.clientSize = function () {}
+File.prototype.clientSize = function () {
+  return this.file['size'];
+}
+
+
+/**
+ * returns temporary path for a given file
+ * @return {String}
+ */
+File.prototype.tmpPath = function () {
+  return this.file['path'];
+}
+
+
+/**
+ * tells whether file has been moved successfully or not
+ * @return {Boolean}
+ */
+File.prototype.moved = function () {
+  return !this.errors()
+}
+
 
 /**
  * returns upload errors if any
  * @return {Object}
  */
-File.prototype.errors = function () {}
+File.prototype.errors = function () {
+  return this.file.error || null
+}
 
 module.exports = File
---
+++ test/unit/file.spec.js
@@ -0,0 +1,81 @@
+"use strict";
+
+require('jasmine-expect');
+const File = require("../../src/File/index");
+const path = require("path")
+const co = require("co")
+
+let dummyFileObject = { 
+  size: 235,
+  path: '/var/folders/gc/lwgzcqtd60vf6z6n1j3trwp40000gn/T/upload_59b36c74cda4c429a01833ab7d6ef547',
+  name: 'npm-logo.svg',
+  type: 'image/svg+xml',
+  mtime: "Mon Aug 17 2015 13:14:21 GMT+0530 (IST)",
+  length: undefined,
+  filename: undefined,
+  mime: undefined 
+}
+
+describe("Files", function() {
+
+  it("should return mimtype for a given file",function(){
+    var file = new File(dummyFileObject);
+    expect(file.mimeType()).toBe(dummyFileObject.type)
+  });
+
+
+  it("should return extension for a given file",function(){
+    var file = new File(dummyFileObject);
+    expect(file.extension()).toBe('svg')
+  });
+
+
+  it("should return client name for the uploaded file",function(){
+    var file = new File(dummyFileObject);
+    expect(file.clientName()).toBe(dummyFileObject.name)
+  });
+
+
+  it("should return size for a given file",function(){
+    var file = new File(dummyFileObject);
+    expect(file.clientSize()).toBe(dummyFileObject.size)
+  });
+
+  it("should return temporary path for a file",function(){
+    var file = new File(dummyFileObject);
+    expect(file.tmpPath()).toBe(dummyFileObject.path)
+  });
+
+  it("should move file to a given path",function(done){
+    var file = new File(dummyFileObject);
+
+    co(function *(){
+      return yield file.move(path.join(__dirname,"./uploads"))
+    }).then(function(success){
+
+      expect(file.moved()).toBe(true)
+      expect(file.errors()).toBe(null)
+
+      done();
+    }).catch(done)
+
+  });
+
+  it("should return an error when unable to move file to a given path",function(done){
+    dummyFileObject.path = 'abc'
+    var file = new File(dummyFileObject);
+
+    co(function *(){
+      return yield file.move(path.join(__dirname,"./uploads"))
+    }).then(function(success){
+
+      expect(file.moved()).toBe(false)
+      expect(file.errors().code).toBe('ENOENT')
+
+      done();
+    }).catch(done)
+
+  });
+
+
+});
