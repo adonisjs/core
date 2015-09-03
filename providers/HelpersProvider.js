4007291030fdf3d7e4cc246783a98ab7cfeff16c
Harminder Virk <virk.officials@gmail.com>
2015-09-03 15:46:27 +0530

    Fixed typo error

--- providers/HelpersProvider.js
+++ providers/HelpersProvider.js
@@ -5,7 +5,7 @@ const ServiceProvider = require('fold').ServiceProvider
 class HelpersProvider extends ServiceProvider{
 
   *register(){
-    this.app.singleton('Adonis/Src/Helpers',function){
+    this.app.singleton('Adonis/Src/Helpers',function(){
       return require('../src/Helpers')
     })
   }
