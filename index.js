95285ec6deeb58835eb8106f010cc09791cd0d3e
Harminder Virk <virk.officials@gmail.com>
2015-12-20 01:00:31 +0530

    Also made make global

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ const ServiceProvider = require('./src/ServiceProvider')
 const Registrar = require('./src/Registrar')
 
 GLOBAL.use = Ioc.use
-GLOBAL.use = Ioc.make
+GLOBAL.make = Ioc.make
 
 module.exports = { Ioc, ServiceProvider, Registrar }
