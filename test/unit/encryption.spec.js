70f7da045f8741be9c0cb313d284b77fdfa04b6a
Richard Kuhnt <r15ch13+git@gmail.com>
2016-03-07 13:07:59 +0100

    Encryption fails on longer content

--- test/unit/encryption.spec.js
+++ test/unit/encryption.spec.js
@@ -38,15 +38,15 @@ describe('Encryption', function() {
 
   it('should encryption values using defined algorithm', function () {
     const cipher = crypto.createCipher(Config.get('app.encryption.algorithm'),Config.get('app.appKey'))
-    cipher.update('virk')
-    const encrypted = encryption.encrypt('virk')
+    cipher.update('These Aren\'t the Droids You\'re Looking For')
+    const encrypted = encryption.encrypt('These Aren\'t the Droids You\'re Looking For')
     expect(encrypted).to.equal(cipher.final('hex'))
   })
 
   it('should decrypt values using defined algorithm', function () {
-    const encrypted = encryption.encrypt('virk')
+    const encrypted = encryption.encrypt('These Aren\'t the Droids You\'re Looking For')
     const decrypted = encryption.decrypt(encrypted)
-    expect(decrypted).to.equal('virk')
+    expect(decrypted).to.equal('These Aren\'t the Droids You\'re Looking For')
   })
 
   it('should throw error when decryption fails', function () {
