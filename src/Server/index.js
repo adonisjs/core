4aa4f8035575d02b285b9ffe3133b089a84c5e14
Harminder Virk <virk.officials@gmail.com>
2016-03-30 23:51:34 +0530

    fix(server): Fix for #128
    Add custom check to check for static resources when request method is GET or HEAD
    
    #128

--- src/Server/index.js
+++ src/Server/index.js
@@ -91,6 +91,11 @@ class Server {
       self._finalHandler(resolvedRoute, request, response)
     }
 
+    if (method !== 'GET' && method !== 'HEAD') {
+      helpers.respondRequest(this.middleware, request, response, finalHandler)
+      return
+    }
+
     this._staticHandler(request, response)
     .catch((e) => {
       if (e.status === 404) {
