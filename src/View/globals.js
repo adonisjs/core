f273a8a4ef6d1ae51c7c7d052f9a3a1d9bccd64e
Juan R Viamonte <pikitgb@gmail.com>
2017-01-28 13:40:33 -0500

    Included html attributes for linkTo and linkToAction helpers definition

--- src/View/globals.js
+++ src/View/globals.js
@@ -14,15 +14,25 @@ const Form = require('./Form')
 module.exports = function (env, Route) {
   env.addGlobal('form', new Form(env, Route))
 
-  env.addGlobal('linkTo', function (route, text, options, target) {
+  env.addGlobal('linkTo', function (route, text, options, target, html) {
     const url = env.filters.route(route, options)
     target = target ? `target="${target}"` : ''
-    return env.filters.safe(`<a href="${url}" ${target}> ${text} </a>`)
+    var props = ""
+    for (var prop_key in html) {
+      const new_prop = (prop_key + "=" + "\"" + html[prop_key]  + "\"" )
+      props = (props + new_prop + " ")
+    }
+    return env.filters.safe(`<a href="${url}" ${target} ${props}> ${text} </a>`)
   })
 
-  env.addGlobal('linkToAction', function (action, text, options, target) {
+  env.addGlobal('linkToAction', function (action, text, options, target, html) {
     const url = env.filters.action(action, options)
     target = target ? `target="${target}"` : ''
-    return env.filters.safe(`<a href="${url}" ${target}> ${text} </a>`)
+    var props = ""
+    for (var prop_key in html) {
+      const new_prop = (prop_key + "=" + "\"" + html[prop_key]  + "\"" )
+      props = (props + new_prop + " ")
+    }
+    return env.filters.safe(`<a href="${url}" ${target} ${props}> ${text} </a>`)
   })
 }
