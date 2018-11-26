97abbb599ece221556d8e175307554320df75050
Harminder Virk <virk.officials@gmail.com>
2018-11-26 15:45:10 +0100

    feat: expose Ioc and Registrar from entry point

---
+++ index.ts
@@ -0,0 +1,15 @@
+/**
+ * @module main
+ */
+
+/*
+* @adonisjs/fold
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+export { Ioc } from './src/Ioc'
+export { Registrar } from './src/Registrar'
