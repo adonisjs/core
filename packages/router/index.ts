ac9aefb506ec35bf641fb7ce1be846a3868254cb
thetutlage <virk.officials@gmail.com>
2019-01-21 18:21:52 +0530

    feat: export router as main module

---
+++ packages/router/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @adonisjs/router
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export { Router } from './src/Router'
