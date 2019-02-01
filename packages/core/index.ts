1827b8c105a696f0607b53c5a2b454eb86ca3083
thetutlage <virk.officials@gmail.com>
2019-02-01 22:02:08 +0530

    chore(core): include providers in list of published files

---
+++ packages/core/index.ts
@@ -0,0 +1,13 @@
+/*
+ * @adonisjs/core
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export { Ignitor } from './src/Ignitor'
+export { ConfigContract } from './src/Contracts/Config'
+export { EnvContract } from './src/Contracts/Env'
+export { HelpersContract } from './src/Contracts/Helpers'
