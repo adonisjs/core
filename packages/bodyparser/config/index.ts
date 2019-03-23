0f383c3e1e6c14821741feeb142e43b24a659a58
thetutlage <virk.officials@gmail.com>
2019-03-23 23:29:12 +0530

    docs(bodyparser): add comments in the config file

--- packages/bodyparser/config/index.ts
+++ packages/bodyparser/config/index.ts
@@ -6,7 +6,26 @@ import { BodyParserConfig } from '../src/Contracts'
  * with the user config
  */
 export const config: BodyParserConfig = {
+  /*
+  |--------------------------------------------------------------------------
+  | White listed methods
+  |--------------------------------------------------------------------------
+  |
+  | HTTP methods for which body parsing must be performed. It is a good practice
+  | to avoid body parsing for `GET` requests.
+  |
+  */
   whitelistedMethods: ['POST', 'PUT', 'PATCH', 'DELETE'],
+
+  /*
+  |--------------------------------------------------------------------------
+  | JSON parser settings
+  |--------------------------------------------------------------------------
+  |
+  | The settings for the JSON parser. The types defines the request content
+  | types which gets processed by the JSON parser.
+  |
+  */
   json: {
     encoding: 'utf-8',
     limit: '1mb',
@@ -18,6 +37,16 @@ export const config: BodyParserConfig = {
       'application/csp-report',
     ],
   },
+
+  /*
+  |--------------------------------------------------------------------------
+  | Form parser settings
+  |--------------------------------------------------------------------------
+  |
+  | The settings for the `application/x-www-form-urlencoded` parser. The types
+  | defines the request content types which gets processed by the form parser.
+  |
+  */
   form: {
     encoding: 'utf-8',
     limit: '1mb',
@@ -26,6 +55,17 @@ export const config: BodyParserConfig = {
       'application/x-www-form-urlencoded',
     ],
   },
+
+  /*
+  |--------------------------------------------------------------------------
+  | Raw body parser settings
+  |--------------------------------------------------------------------------
+  |
+  | Raw body just reads the request body stream as a plain text, which you
+  | can process by hand. This must be used when request body type is not
+  | supoorted by bodyparser.
+  |
+  */
   raw: {
     encoding: 'utf-8',
     limit: '1mb',
@@ -34,9 +74,52 @@ export const config: BodyParserConfig = {
       'text/*',
     ],
   },
+
+  /*
+  |--------------------------------------------------------------------------
+  | Multipart parser settings
+  |--------------------------------------------------------------------------
+  |
+  | The settings for the `multipart/form-data` parser. The types defines the
+  | request content types which gets processed by the form parser.
+  |
+  */
   multipart: {
+    /*
+    |--------------------------------------------------------------------------
+    | Auto process
+    |--------------------------------------------------------------------------
+    |
+    | The auto process option will process uploaded files and writes them to
+    | the `tmp` folder. You can turn it off and then manually use the stream
+    | to pipe stream to a different destination.
+    |
+    | It is recommended to keep `autoProcess=true`. Unless you are processing bigger
+    | file sizes.
+    |
+    */
     autoProcess: true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Files to be processed manually
+    |--------------------------------------------------------------------------
+    |
+    | You can turn off `autoProcess` for certain routes as well by defining
+    | routes inside the following array.
+    |
+    */
     processManually: [],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Temporary file name
+    |--------------------------------------------------------------------------
+    |
+    | When auto processing is on. We will use this method to compute the temporary
+    | file name.
+    |
+    */
     tmpFileName () {
       return `ab-${uuid()}.tmp`
     },
