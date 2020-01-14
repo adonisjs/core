ae77df5d7ede10a8e697d77e78057fc22f11cb63
Harminder virk <virk.officials@gmail.com>
2020-01-14 09:32:26 +0530

    chore: add forceContentNegotiationToJSON to config/app file

--- instructions.ts
+++ instructions.ts
@@ -7,14 +7,33 @@
  * file that was distributed with this source code.
 */
 
+import { join } from 'path'
 import * as sinkStatic from '@adonisjs/sink'
 import { ApplicationContract } from '@ioc:Adonis/Core/Application'
 
 export default async function instructions (
   projectRoot: string,
   application: ApplicationContract,
-  { executeInstructions }: typeof sinkStatic,
+  { executeInstructions, TemplateFile, logger }: typeof sinkStatic,
 ) {
+  const template = new TemplateFile(
+    projectRoot,
+    'config/app.ts',
+    join(__dirname, './config', 'app.txt'),
+  )
+
+  if (template.exists()) {
+    logger.skip('config/app.ts')
+  } else {
+    template.apply({
+      forceContentNegotiationToJSON: process.env['ADONIS_CREATE_APP_BOILERPLATE'] === 'api'
+        ? true
+        : false,
+    })
+    template.commit()
+    logger.create('config/app.ts')
+  }
+
   await executeInstructions('@adonisjs/events', projectRoot, application)
   await executeInstructions('@adonisjs/hash', projectRoot, application)
   await executeInstructions('@adonisjs/validator', projectRoot, application)
