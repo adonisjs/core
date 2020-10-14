817ee8dae0e6dbc421ad95fdbb5277f898046b81
Harminder Virk <virk.officials@gmail.com>
2020-10-14 11:09:48 +0530

    chore: update instructions to create env validations and .env file

--- instructions.ts
+++ instructions.ts
@@ -8,12 +8,17 @@
  */
 
 import { join } from 'path'
+import { randomString } from '@poppinss/utils'
 import * as sinkStatic from '@adonisjs/sink'
 import { ApplicationContract } from '@ioc:Adonis/Core/Application'
 
-const APP_TEMPLATE_STUB = join(__dirname, './templates', 'config', 'app.txt')
+const ENV_VALIDATIONS_TEMPLATE_STUB = join(__dirname, './templates', 'env.txt')
 const STATIC_TEMPLATE_STUB = join(__dirname, './templates', 'config', 'static.txt')
+const APP_CONFIG_TEMPLATE_STUB = join(__dirname, './templates', 'config', 'app.txt')
 
+/**
+ * Configure package
+ */
 export default async function instructions(
 	projectRoot: string,
 	_: ApplicationContract,
@@ -24,7 +29,7 @@ export default async function instructions(
 	/**
 	 * Create app config file
 	 */
-	const appConfig = new files.MustacheFile(projectRoot, 'config/app.ts', APP_TEMPLATE_STUB)
+	const appConfig = new files.MustacheFile(projectRoot, 'config/app.ts', APP_CONFIG_TEMPLATE_STUB)
 	if (appConfig.exists()) {
 		logger.action('create').skipped('config/app.ts', 'File already exists')
 	} else {
@@ -33,8 +38,7 @@ export default async function instructions(
 	}
 
 	/**
-	 * Create static config file when boilerplate
-	 * is not for the api
+	 * Create static config file when boilerplate is not for the api
 	 */
 	if (!isApiBoilerplate) {
 		const staticConfig = new files.MustacheFile(
@@ -48,5 +52,39 @@ export default async function instructions(
 			staticConfig.apply({}).commit()
 			logger.action('create').succeeded('config/static.ts')
 		}
+	}
+
+	/**
+	 * Add `public` folder to the meta files when not selected api
+	 * boilerplate
+	 */
+	if (!isApiBoilerplate) {
+		const rcFile = new files.AdonisRcFile(projectRoot)
+		rcFile.addMetaFile('public/**', false)
+		rcFile.commit()
+		logger.action(rcFile.exists() ? 'update' : 'create').succeeded('.adonisrc.json')
+	}
+
+	/**
+	 * Create .env file for holding environment variables during
+	 * development
+	 */
+	const env = new files.EnvFile(projectRoot)
+	env.set('PORT', 3333)
+	env.set('HOST', '0.0.0.0')
+	env.set('PORT', 'development')
+	env.set('APP_KEY', randomString(32))
+	env.commit()
+	logger.action(env.exists() ? 'update' : 'create').succeeded('.env,.env.example')
+
+	/**
+	 * Create env.ts file for performing environment variable validations
+	 */
+	const envTsFile = new files.MustacheFile(projectRoot, 'env.ts', ENV_VALIDATIONS_TEMPLATE_STUB)
+	if (envTsFile.exists()) {
+		logger.action('create').skipped('env.ts')
+	} else {
+		envTsFile.apply({}).commit()
+		logger.action('create').succeeded('env.ts')
 	}
 }
--- templates/config/hash.txt
+++ templates/config/hash.txt
@@ -27,7 +27,7 @@ const hashConfig: HashConfig = {
   | free to change the default value
   |
   */
-  default: Env.get('HASH_DRIVER', 'argon') as 'argon',
+  default: Env.get('HASH_DRIVER', 'argon'),
 
   list: {
     /*
