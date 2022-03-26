d7bccc57c68792e514ea563f471327d3bf8e07ce
Harminder Virk <virk.officials@gmail.com>
2022-03-26 11:07:34 +0530

    refactor: UI improvements for the list:routes command. Both the pretty output and the table output (#3645)
    * improvement: ui updates for the pretty and the table renderer
    
    * refactor: cleanup list routes command code

--- test/commands/list-routes-json.spec.ts
+++ test/commands/list-routes-json.spec.ts
@@ -16,7 +16,7 @@ import { Application } from '@adonisjs/application'
 import { Router } from '@adonisjs/http-server/build/src/Router'
 import { PreCompiler } from '@adonisjs/http-server/build/src/Server/PreCompiler/index'
 
-import ListRoutes from '../../commands/ListRoutes/ListRoutes'
+import ListRoutes from '../../commands/ListRoutes'
 
 const ioc = new Ioc()
 const precompiler = new PreCompiler(ioc, {
@@ -31,7 +31,7 @@ test.group('Command | List Routes Json', (group) => {
     testingRenderer.logs = []
   })
 
-  test('list routes in the order they are register', async ({ assert }) => {
+  test('list routes in the order they are registered', async ({ assert }) => {
     const app = new Application(__dirname, 'test', {})
 
     const router = new Router({} as any, precompiler.compileRoute.bind(precompiler))
@@ -53,17 +53,17 @@ test.group('Command | List Routes Json', (group) => {
           root: [
             {
               domain: 'root',
-              methods: ['HEAD', 'GET'],
               name: '',
               pattern: '/about',
+              methods: ['GET', 'HEAD'],
               handler: 'Closure',
               middleware: [],
             },
             {
               domain: 'root',
-              methods: ['HEAD', 'GET'],
               name: '',
               pattern: '/contact',
+              methods: ['GET', 'HEAD'],
               handler: 'Closure',
               middleware: [],
             },
@@ -73,7 +73,7 @@ test.group('Command | List Routes Json', (group) => {
     )
   })
 
-  test('list routes with assigned middleware', async ({ assert }) => {
+  test('list routes with middleware', async ({ assert }) => {
     const app = new Application(__dirname, 'test', {})
 
     const router = new Router({} as any, precompiler.compileRoute.bind(precompiler))
@@ -95,17 +95,17 @@ test.group('Command | List Routes Json', (group) => {
           root: [
             {
               domain: 'root',
-              methods: ['HEAD', 'GET'],
               name: '',
               pattern: '/about',
+              methods: ['GET', 'HEAD'],
               handler: 'Closure',
               middleware: [],
             },
             {
               domain: 'root',
-              methods: ['HEAD', 'GET'],
               name: '',
               pattern: '/contact',
+              methods: ['GET', 'HEAD'],
               handler: 'Closure',
               middleware: ['auth', 'acl:admin'],
             },
@@ -140,17 +140,17 @@ test.group('Command | List Routes Json', (group) => {
           root: [
             {
               domain: 'root',
-              methods: ['HEAD', 'GET'],
-              pattern: '/about',
               name: '',
+              pattern: '/about',
+              methods: ['GET', 'HEAD'],
               handler: 'HomeController.index',
               middleware: [],
             },
             {
               domain: 'root',
-              methods: ['HEAD', 'GET'],
-              pattern: '/contact',
               name: '',
+              pattern: '/contact',
+              methods: ['GET', 'HEAD'],
               handler: 'ContactController.handle',
               middleware: [],
             },
@@ -185,18 +185,63 @@ test.group('Command | List Routes Json', (group) => {
           root: [
             {
               domain: 'root',
-              methods: ['HEAD', 'GET'],
-              pattern: '/about',
               name: '',
+              pattern: '/about',
+              methods: ['GET', 'HEAD'],
               handler: 'HomeController.index',
               middleware: [],
             },
             {
               domain: 'root',
-              methods: ['HEAD', 'GET'],
-              pattern: '/contact',
               name: '',
+              pattern: '/contact',
+              methods: ['GET', 'HEAD'],
               handler: 'App/Admin/ContactController.handle',
+              middleware: [],
+            },
+          ],
+        },
+      ]
+    )
+  })
+
+  test('ignore custom namespace when its same as the default namespace', async ({ assert }) => {
+    const app = new Application(__dirname, 'test', {})
+
+    const router = new Router({} as any, precompiler.compileRoute.bind(precompiler))
+    ioc.bind('App/Controllers/Http/HomeController', () => {})
+    ioc.bind('App/Controllers/Http/ContactController', () => {})
+
+    router.get('about', 'HomeController.index')
+    router.get('contact', 'ContactController').namespace('App/Controllers/Http')
+    router.commit()
+
+    app.container.bind('Adonis/Core/Route', () => router)
+
+    const listRoutes = new ListRoutes(app, new Kernel(app))
+    listRoutes.json = true
+    listRoutes.logger.useRenderer(testingRenderer)
+    await listRoutes.run()
+
+    assert.deepEqual(
+      testingRenderer.logs.map(({ message }) => JSON.parse(message)),
+      [
+        {
+          root: [
+            {
+              domain: 'root',
+              name: '',
+              pattern: '/about',
+              methods: ['GET', 'HEAD'],
+              handler: 'HomeController.index',
+              middleware: [],
+            },
+            {
+              domain: 'root',
+              name: '',
+              pattern: '/contact',
+              methods: ['GET', 'HEAD'],
+              handler: 'ContactController.handle',
               middleware: [],
             },
           ],
@@ -227,10 +272,10 @@ test.group('Command | List Routes Json', (group) => {
           'blogger.com': [
             {
               domain: 'blogger.com',
-              methods: ['HEAD', 'GET'],
-              pattern: '/about',
-              handler: 'Closure',
               name: '',
+              pattern: '/about',
+              methods: ['GET', 'HEAD'],
+              handler: 'Closure',
               middleware: [],
             },
           ],
@@ -265,13 +310,148 @@ test.group('Command | List Routes Json', (group) => {
           'blogger.com': [
             {
               domain: 'blogger.com',
-              methods: ['HEAD', 'GET'],
-              pattern: '/v1/about',
-              handler: 'Closure',
               name: '',
+              pattern: '/v1/about',
+              methods: ['GET', 'HEAD'],
+              handler: 'Closure',
               middleware: [],
             },
           ],
+        },
+      ]
+    )
+  })
+
+  test('filter routes by method', async ({ assert }) => {
+    const app = new Application(__dirname, 'test', {})
+
+    const router = new Router({} as any, precompiler.compileRoute.bind(precompiler))
+    router.get('about', async () => {})
+    router.post('contact', async () => {})
+    router.commit()
+
+    app.container.bind('Adonis/Core/Route', () => router)
+
+    const listRoutes = new ListRoutes(app, new Kernel(app))
+    listRoutes.logger.useRenderer(testingRenderer)
+    listRoutes.json = true
+    listRoutes.methodFilter = 'GET'
+    await listRoutes.run()
+
+    assert.deepEqual(
+      testingRenderer.logs.map(({ message }) => JSON.parse(message)),
+      [
+        {
+          root: [
+            {
+              domain: 'root',
+              name: '',
+              pattern: '/about',
+              methods: ['GET', 'HEAD'],
+              handler: 'Closure',
+              middleware: [],
+            },
+          ],
+        },
+      ]
+    )
+  })
+
+  test('filter routes by name', async ({ assert }) => {
+    const app = new Application(__dirname, 'test', {})
+
+    const router = new Router({} as any, precompiler.compileRoute.bind(precompiler))
+    router.get('about', async () => {})
+    router.post('contact', async () => {}).as('contactUs')
+    router.commit()
+
+    app.container.bind('Adonis/Core/Route', () => router)
+
+    const listRoutes = new ListRoutes(app, new Kernel(app))
+    listRoutes.logger.useRenderer(testingRenderer)
+    listRoutes.json = true
+    listRoutes.nameFilter = 'contactUs'
+    await listRoutes.run()
+
+    assert.deepEqual(
+      testingRenderer.logs.map(({ message }) => JSON.parse(message)),
+      [
+        {
+          root: [
+            {
+              domain: 'root',
+              name: 'contactUs',
+              pattern: '/contact',
+              methods: ['POST'],
+              handler: 'Closure',
+              middleware: [],
+            },
+          ],
+        },
+      ]
+    )
+  })
+
+  test('filter routes by route pattern', async ({ assert }) => {
+    const app = new Application(__dirname, 'test', {})
+
+    const router = new Router({} as any, precompiler.compileRoute.bind(precompiler))
+    router.get('about', async () => {})
+    router.post('contact', async () => {})
+    router.commit()
+
+    app.container.bind('Adonis/Core/Route', () => router)
+
+    const listRoutes = new ListRoutes(app, new Kernel(app))
+    listRoutes.logger.useRenderer(testingRenderer)
+    listRoutes.json = true
+    listRoutes.patternFilter = '/ab'
+    await listRoutes.run()
+
+    assert.deepEqual(
+      testingRenderer.logs.map(({ message }) => JSON.parse(message)),
+      [
+        {
+          root: [
+            {
+              domain: 'root',
+              name: '',
+              pattern: '/about',
+              methods: ['GET', 'HEAD'],
+              handler: 'Closure',
+              middleware: [],
+            },
+          ],
+        },
+      ]
+    )
+  })
+
+  test('apply a combination of filters', async ({ assert }) => {
+    const app = new Application(__dirname, 'test', {})
+
+    const router = new Router({} as any, precompiler.compileRoute.bind(precompiler))
+    router.get('about', async () => {})
+    router.post('about', async () => {})
+    router.put('about', async () => {}).as('editDetails')
+    router.get('contact', async () => {})
+    router.commit()
+
+    app.container.bind('Adonis/Core/Route', () => router)
+
+    const listRoutes = new ListRoutes(app, new Kernel(app))
+    listRoutes.logger.useRenderer(testingRenderer)
+    listRoutes.json = true
+    listRoutes.patternFilter = '/ab'
+    listRoutes.methodFilter = 'GET'
+    listRoutes.nameFilter = 'editDetails'
+    await listRoutes.run()
+
+    assert.deepEqual(
+      testingRenderer.logs.map(({ message }) => JSON.parse(message)),
+      [
+        {
+          root: [],
         },
       ]
     )
--- test/commands/list-routes-pretty.spec.ts
+++ test/commands/list-routes-pretty.spec.ts
@@ -7,19 +7,19 @@
  * file that was distributed with this source code.
  */
 
-import { test } from '@japa/runner'
 import 'reflect-metadata'
+import stripAnsi from 'strip-ansi'
+import { test } from '@japa/runner'
 import { Ioc } from '@adonisjs/fold'
 import { Kernel } from '@adonisjs/ace'
 import { testingRenderer } from '@poppinss/cliui'
 import { Application } from '@adonisjs/application'
-import { Router } from '@adonisjs/http-server/build/src/Router'
-import { PreCompiler } from '@adonisjs/http-server/build/src/Server/PreCompiler/index'
-import stripAnsi from 'strip-ansi'
-
-import ListRoutes from '../../commands/ListRoutes/ListRoutes'
-import { ApplicationContract } from '@ioc:Adonis/Core/Application'
 import { RouterContract } from '@ioc:Adonis/Core/Route'
+import { Router } from '@adonisjs/http-server/build/src/Router'
+import { ApplicationContract } from '@ioc:Adonis/Core/Application'
+import { PreCompiler } from '@adonisjs/http-server/build/src/Server/PreCompiler/index'
+
+import ListRoutes from '../../commands/ListRoutes'
 
 const ioc = new Ioc()
 const precompiler = new PreCompiler(ioc, {
@@ -58,8 +58,8 @@ test.group('Command | List Routes Pretty', (group) => {
 
     const output = testingRenderer.logs.map(({ message }) => stripAnsi(message))
     assert.deepEqual(output, [
-      'HEAD|GET    /about ....................... Closure',
-      'POST        /contact ..................... Closure',
+      'GET|HEAD    /about ─────────────────────── Closure',
+      'POST        /contact ───────────────────── Closure',
     ])
   })
 
@@ -77,10 +77,10 @@ test.group('Command | List Routes Pretty', (group) => {
 
     const output = testingRenderer.logs.map(({ message }) => stripAnsi(message))
     assert.deepEqual(output, [
-      'HEAD|GET    /about ....................... Closure',
-      'POST        /contact ......... TestController.test',
+      'GET|HEAD    /about ─────────────────────── Closure',
+      'POST        /contact ───────── TestController.test',
       'POST        /my-super-long-route-name  AdonisTest…',
-      'POST        /end .. AdonisTestControllerTest.index',
+      'POST        /end ── AdonisTestControllerTest.index',
     ])
   })
 
@@ -99,12 +99,12 @@ test.group('Command | List Routes Pretty', (group) => {
 
     const output = testingRenderer.logs.map(({ message }) => stripAnsi(message))
     assert.deepEqual(output, [
-      'HEAD|GET    /about ....................... Closure',
-      'POST        /contact ......... TestController.test',
-      '            ⇂ throttle:10,1',
+      'GET|HEAD    /about ─────────────────────── Closure',
+      'POST        /contact ───────── TestController.test',
+      '             ├── throttle:10,1',
       'POST        /my-super-long-route-name  AdonisTest…',
-      'POST        /end .. AdonisTestControllerTest.index',
-      '            ⇂ auth',
+      'POST        /end ── AdonisTestControllerTest.index',
+      '             ├── auth',
     ])
   })
 })
