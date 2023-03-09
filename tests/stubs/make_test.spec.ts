64ec44e3c451ca0fc245f9591c674556d79391bb
Harminder Virk <virk.officials@gmail.com>
2023-03-09 15:15:47 +0530

    feat: add make:test command

---
+++ tests/stubs/make_test.spec.ts
@@ -0,0 +1,39 @@
+/*
+ * @adonisjs/core
+ *
+ * (c) AdonisJS
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { join } from 'node:path'
+import { test } from '@japa/runner'
+import { fileURLToPath } from 'node:url'
+import { AppFactory } from '@adonisjs/application/factories'
+
+import { stubsRoot } from '../../stubs/index.js'
+
+const BASE_URL = new URL('./tmp/', import.meta.url)
+const BASE_PATH = fileURLToPath(BASE_URL)
+
+test.group('Make test', () => {
+  test('prepare test stub', async ({ assert }) => {
+    const app = new AppFactory().create(BASE_URL, () => {})
+    await app.init()
+
+    const stub = await app.stubs.build('make/test/main.stub', {
+      source: stubsRoot,
+    })
+    const { contents, destination } = await stub.prepare({
+      entity: app.generators.createEntity('users/send_email'),
+      suite: {
+        directory: 'tests/functional',
+      },
+    })
+
+    assert.equal(destination, join(BASE_PATH, 'tests/functional/users/send_email.spec.ts'))
+    assert.match(contents, new RegExp(`import { test } from '@japa/runner'`))
+    assert.match(contents, new RegExp(`test\\.group\\('Users send email'`))
+  })
+})
