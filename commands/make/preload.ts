d590a31ec307ccc4fd9ac388da499696a164f0f2
Julien Ripouteau <julien@ripouteau.com>
2023-07-08 13:18:42 +0200

    feat: prompt environment in make:preload file (#4187)

---
+++ commands/make/preload.ts
@@ -0,0 +1,112 @@
+/*
+ * @adonisjs/core
+ *
+ * (c) AdonisJS
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import BaseCommand from './_base.js'
+import { args, flags } from '../../modules/ace/main.js'
+import type { AppEnvironments } from '@adonisjs/application/types'
+
+const ALLOWED_ENVIRONMENTS: AppEnvironments[] = ['web', 'console', 'test', 'repl']
+
+/**
+ * Make a new preload file
+ */
+export default class MakePreload extends BaseCommand {
+  static commandName = 'make:preload'
+  static description = 'Create a new preload file inside the start directory'
+
+  @args.string({ description: 'Name of the preload file' })
+  declare name: string
+
+  @flags.array({
+    description: `Define the preload file's environment. Accepted values are "${ALLOWED_ENVIRONMENTS}"`,
+  })
+  declare environments: AppEnvironments[]
+
+  /**
+   * The stub to use for generating the preload file
+   */
+  protected stubPath: string = 'make/preload_file/main.stub'
+
+  /**
+   * Check if the mentioned environments are valid
+   */
+  #isValidEnvironment(environment: string[]): environment is AppEnvironments[] {
+    return !environment.find((one) => !ALLOWED_ENVIRONMENTS.includes(one as any))
+  }
+
+  /**
+   * Validate the environments flag passed by the user
+   */
+  #isEnvironmentsFlagValid() {
+    if (!this.environments || !this.environments.length) {
+      return true
+    }
+
+    return this.#isValidEnvironment(this.environments)
+  }
+
+  /**
+   * Prompt for the environments
+   */
+  async #promptForEnvironments(): Promise<AppEnvironments[]> {
+    const selectedEnvironments = await this.prompt.multiple(
+      'Select the environment(s) in which you want to load this file',
+      [
+        { name: 'all', message: 'Load file in all environments' },
+        { name: 'console', message: 'Environment for ace commands' },
+        { name: 'repl', message: 'Environment for the REPL session' },
+        { name: 'web', message: 'Environment for HTTP requests' },
+        { name: 'test', message: 'Environment for the test process' },
+      ] as const
+    )
+
+    if (selectedEnvironments.includes('all')) {
+      return ['web', 'console', 'test', 'repl']
+    }
+
+    return selectedEnvironments as AppEnvironments[]
+  }
+
+  /**
+   * Run command
+   */
+  async run() {
+    let environments: AppEnvironments[] = this.environments
+
+    /**
+     * Ensure the environments are valid when provided via flag
+     */
+    if (!this.#isEnvironmentsFlagValid()) {
+      this.logger.error(
+        `Invalid environment(s) "${this.environments}". Only "${ALLOWED_ENVIRONMENTS}" are allowed`
+      )
+      return
+    }
+
+    /**
+     * Prompt for the environments when not defined
+     */
+    if (!environments) {
+      environments = await this.#promptForEnvironments()
+    }
+
+    const output = await this.generate(this.stubPath, {
+      entity: this.app.generators.createEntity(this.name),
+    })
+
+    /**
+     * Registering the preload file with the `.adonisrc.json` file. We register
+     * the relative path, since we cannot be sure about aliases to exist.
+     */
+    const preloadImportPath = `./${output.relativeFileName.replace(/(\.js|\.ts)$/, '')}.js`
+    await this.app.rcFileEditor
+      .addPreloadFile(preloadImportPath, environments as Exclude<AppEnvironments, 'unknown'>[])
+      .save()
+  }
+}
---
+++ tests/commands/make_preload.spec.ts
@@ -0,0 +1,78 @@
+/*
+ * @adonisjs/core
+ *
+ * (c) AdonisJS
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { test } from '@japa/runner'
+import { StubsFactory } from '../../factories/stubs.js'
+import { AceFactory } from '../../factories/core/ace.js'
+import MakePreload from '../../commands/make/preload.js'
+
+test.group('Make preload file', () => {
+  test('create preload file', async ({ assert, fs }) => {
+    const ace = await new AceFactory().make(fs.baseUrl, {
+      importer: (filePath) => import(filePath),
+    })
+    await ace.app.init()
+    ace.ui.switchMode('raw')
+
+    const command = await ace.create(MakePreload, ['app', '--environments=web'])
+    await command.exec()
+
+    const { contents } = await new StubsFactory().prepare('make/preload_file/main.stub', {
+      entity: ace.app.generators.createEntity('app'),
+    })
+    await assert.fileEquals('start/app.ts', contents)
+
+    assert.deepEqual(ace.ui.logger.getLogs(), [
+      {
+        message: 'green(DONE:)    create start/app.ts',
+        stream: 'stdout',
+      },
+    ])
+
+    await assert.fileContains('.adonisrc.json', /"\.\/start\/app\.js"/)
+  })
+
+  test('create preload file for specific environments', async ({ assert, fs }) => {
+    const ace = await new AceFactory().make(fs.baseUrl, {
+      importer: (filePath) => import(filePath),
+    })
+    await ace.app.init()
+    ace.ui.switchMode('raw')
+
+    const command = await ace.create(MakePreload, ['app'])
+    command.prompt
+      .trap('Select the environment(s) in which you want to load this file')
+      .replyWith(['web', 'repl'])
+
+    await command.exec()
+
+    const rcFile = await fs.contentsJson('.adonisrc.json')
+
+    assert.deepEqual(rcFile.preloads, [{ file: './start/app.js', environment: ['web', 'repl'] }])
+  })
+
+  test('create preload file for all environments', async ({ assert, fs }) => {
+    const ace = await new AceFactory().make(fs.baseUrl, {
+      importer: (filePath) => import(filePath),
+    })
+    await ace.app.init()
+    ace.ui.switchMode('raw')
+
+    const command = await ace.create(MakePreload, ['app'])
+    command.prompt
+      .trap('Select the environment(s) in which you want to load this file')
+      .replyWith(['all'])
+
+    await command.exec()
+
+    const rcFile = await fs.contentsJson('.adonisrc.json')
+
+    assert.deepEqual(rcFile.preloads, ['./start/app.js'])
+  })
+})
