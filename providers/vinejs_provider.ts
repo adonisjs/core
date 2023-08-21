b0a097a1816a11eb3a348889510d3240f8c7f706
Harminder Virk <virk.officials@gmail.com>
2023-08-21 12:47:53 +0530

    fix: remove app reference when not used

--- providers/vinejs_provider.ts
+++ providers/vinejs_provider.ts
@@ -7,11 +7,7 @@
  * file that was distributed with this source code.
  */
 
-import type { ApplicationService } from '../src/types.js'
-
 import '../src/bindings/vinejs.js'
 import '../modules/http/request_validator.js'
 
-export default class VineJSServiceProvider {
-  constructor(protected app: ApplicationService) {}
-}
+export default class VineJSServiceProvider {}
