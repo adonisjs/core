97c822bb4ab482980b96c0af5b91d7c9c827353a
Guo Yunhe <i@guoyunhe.me>
2026-02-01 15:15:49 +0800

    refactor(tests): format log messages and improve test readability in lang publish tests

--- tests/commands/lang_publish.spec.ts
+++ tests/commands/lang_publish.spec.ts
@@ -1,4 +1,3 @@
-
 import { test } from '@japa/runner'
 import { AceFactory } from '../../factories/core/ace.js'
 import LangPublishCommand from '../../commands/lang/publish.js'
@@ -20,7 +19,8 @@ test.group('Lang publish', () => {
 
     assert.deepEqual(ace.ui.logger.getLogs(), [
       {
-        message: '[ green(success) ] Localization template published to resources/lang/en/validator.json',
+        message:
+          '[ green(success) ] Localization template published to resources/lang/en/validator.json',
         stream: 'stdout',
       },
     ])
@@ -41,21 +41,28 @@ test.group('Lang publish', () => {
 
     assert.deepEqual(ace.ui.logger.getLogs(), [
       {
-        message: '[ red(error) ] File "resources/lang/en/validator.json" already exists. Use "--merge" flag to update the file',
+        message:
+          '[ red(error) ] File "resources/lang/en/validator.json" already exists. Use "--merge" flag to update the file',
         stream: 'stderr',
       },
     ])
   })
 
-  test('merge when validator messages file already exists and --merge is used', async ({ assert, fs }) => {
+  test('merge when validator messages file already exists and --merge is used', async ({
+    assert,
+    fs,
+  }) => {
     const ace = await new AceFactory().make(fs.baseUrl)
     await ace.app.init()
     ace.ui.switchMode('raw')
 
-    await fs.create('resources/lang/en/validator.json', JSON.stringify({
-      shared: { messages: { required: 'foo' } },
-      other: true
-    }))
+    await fs.create(
+      'resources/lang/en/validator.json',
+      JSON.stringify({
+        shared: { messages: { required: 'foo' } },
+        other: true,
+      })
+    )
 
     const command = await ace.create(LangPublishCommand, ['--merge'])
     await command.exec()
@@ -70,7 +77,8 @@ test.group('Lang publish', () => {
 
     assert.deepEqual(ace.ui.logger.getLogs(), [
       {
-        message: '[ green(success) ] Localization template published to resources/lang/en/validator.json',
+        message:
+          '[ green(success) ] Localization template published to resources/lang/en/validator.json',
         stream: 'stdout',
       },
     ])
