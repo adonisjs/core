d83553ae2400999b1eab2e68d191ff032205c736
Harminder Virk <virk.officials@gmail.com>
2026-02-25 15:35:21 +0530

    fix: broken codemod in last commit

--- modules/ace/codemods.ts
+++ modules/ace/codemods.ts
@@ -622,8 +622,8 @@ export class Codemods extends EventEmitter {
     const transformer = await this.#getCodeTransformer()
     const appPath = this.#app.makePath()
     const colors = this.#cliLogger.getColors()
-    const devDependencies = packages.filter((pkg) => pkg.isDevDependency)
-    const dependencies = packages.filter((pkg) => !pkg.isDevDependency)
+    const devDependencies = packages.filter((pkg) => pkg.isDevDependency).map(({ name }) => name)
+    const dependencies = packages.filter((pkg) => !pkg.isDevDependency).map(({ name }) => name)
 
     if (!transformer) {
       this.#cliLogger.warning(
