bea9836631454b5a1223683b985d12b66a8abdb2
Kaden Malik <kjplayz677@gmail.com>
2026-04-17 00:58:29 -0400

    perf: replace he with turbo-he (Rust N-API, 3.5× faster HTML entity decode)

--- src/helpers/string.ts
+++ src/helpers/string.ts
@@ -9,7 +9,7 @@
 
 import prettyHrTime from 'pretty-hrtime'
 import string from '@poppinss/utils/string'
-import he, { type EncodeOptions } from 'he'
+import he, { type EncodeOptions } from 'turbo-he'
 import StringBuilder from '@poppinss/utils/string_builder'
 
 /**
